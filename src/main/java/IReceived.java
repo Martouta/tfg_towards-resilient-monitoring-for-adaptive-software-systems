f6354ff7e78345a6a424d9c9af1d679d1a52fd2b
Marta Noya <tim4martanoya@gmail.com>
2017-11-06 17:44:56 +0100

    IReceived + test

---
+++ src/main/java/IReceived.java
@@ -0,0 +1,3 @@
+interface IReceived { // # TODO: is this.'class' the same as IReceiver?
+    public void doOperation(ObtainedData od); // # TODO: should it be static?
+}
---
+++ src/main/java/Received.java
@@ -0,0 +1,5 @@
+public class Received implements IReceived {
+    public void doOperation(ObtainedData od) {
+        System.out.println(od.toString());
+  	}
+}
